1e627f13407bd6f03baf750cabb9a56cc8049805
YC <53665280+peterchenyipu@users.noreply.github.com>
2021-09-14 09:46:31 +0800

    Changed Library.java

--- src/main/java/Lab1/Library.java
+++ src/main/java/Lab1/Library.java
@@ -5,7 +5,9 @@ package Lab1;
 
 public class Library {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Library lib = new Library();
+        if (lib.someLibraryMethod())
+            System.out.println("Hello World");
     }
 
     public boolean someLibraryMethod() {
